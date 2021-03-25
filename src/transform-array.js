d11245b0ac8f8d3ce04938745ebf900377b8719b
OlgaMoisevich <ol.ivochka@yandex.ru>
2021-03-25 21:17:52 +0300

    feat: add solition 2 task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,42 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) {
+    new Error();
+  }
+  if (arr.length < 0) return [];
+
+  let new_arr = [];
+
+  for (let item = 0; item < arr.length; item++) {
+    switch (arr[item]) {
+      case '--discard-next':
+        if (item < arr.length) {
+          item++;
+          if (arr[item + 1] == "--discard-prev" || arr[item + 1] == "--double-prev") {
+            item++;
+          }
+        }
+        continue;
+      case '--discard-prev':
+        if (item > 0) {
+          new_arr.pop();
+        }
+        continue;
+      case '--double-next':
+        if (item < arr.length - 1) {
+          new_arr.push(arr[item + 1]);
+        }
+        continue;
+      case '--double-prev':
+        if (item > 0) {
+          new_arr.push(arr[item - 1]);
+        }
+        continue;
+      default:
+        new_arr.push(arr[item]);
+        continue;
+    }
+  }
+  return new_arr;
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,32 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (date === undefined) return 'Unable to determine the time of year!';
+  if (isNaN(date.getTime())) {
+    throw new Error();
+  }
+  let month = date.getMonth() + 1;
+
+  switch (month) {
+    case 3:
+    case 4:
+    case 5:
+      return 'spring';
+      break;
+    case 6:
+    case 7:
+    case 8:
+      return 'summer';
+      break;
+    case 9:
+    case 10:
+    case 11:
+      return 'autumn';
+      break;
+    case 12:
+    case 1:
+    case 2:
+      return 'winter';
+      break;
+  }
 };
