b21bf0be6b0198ca5fbcef65505c5657e4c7067e
OlgaMoisevich <ol.ivochka@yandex.ru>
2021-03-29 17:40:14 +0300

    feat: add solution task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,27 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, option) {
+  const {
+    repeatTimes = 1,
+    separator = '+',
+    addition = '',
+    additionRepeatTimes = 1,
+    additionSeparator = '|'
+  } = option;
+
+  let return_arr = [];
+
+  for (let i = 0; i < additionRepeatTimes; i++) {
+    if (i === 0) {
+      return_arr.push(String(str));
+    }
+    return_arr.push(String(addition));
+    if (i !== additionRepeatTimes - 1) {
+      return_arr.push(additionSeparator);
+    } else {
+      return_arr.push(separator);
+    }
+  }
+  return new Array(repeatTimes).fill(return_arr.join('')).join('').slice(0, -separator.length);
 };
-  
+
--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,12 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+  // M n = 2 n -1
+  let min_moov = Math.pow(2, disksNumber)-1;
+
+  return {
+    turns: min_moov,
+    seconds: Math.floor(min_moov/(turnsSpeed/3600))
+  }
+
 };
